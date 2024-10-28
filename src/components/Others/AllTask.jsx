b4bfd561fab1bda48b88ad854c8d0fa80c05fd95
vipin018 <vipinpathak018@gmail.com>
2024-10-28 19:46:56 +0530

    fuked up shiit

--- src/components/Others/AllTask.jsx
+++ src/components/Others/AllTask.jsx
@@ -1,31 +1,32 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
 
+   
     const authData = useContext(AuthContext);
-    console.log(authData.employees);
 
     return (
-        <div id='allTask' className='bg-[#1c1c1c] mt-5 rounded p-5 h-48 overflow-auto'>
+        <div id='allTask' className='bg-[#1c1c1c] mt-5 rounded p-5 '>
 
-            <div className='bg-red-500 mb-2 flex justify-between rounded py-2 px-4'>
-                <h2 className='w-1/5 bg-red-600'>Employee Name</h2>
-                <h3 className='w-1/5 bg-red-600 '>New Task</h3>
-                <h5 className='w-1/5 bg-red-600'>Active Task</h5>
-                <h5 className='w-1/5 bg-red-600'>Completed Task</h5>
-                <h5 className='w-1/5 bg-red-600'>Failed Task</h5>
+            <div className='bg-emerald-500 mb-2 flex justify-between rounded py-2 px-4'>
+                <h3 className='w-1/5 font-medium text-lg '>Employee Name</h3>
+                <h2 className='w-1/5 font-medium text-lg '>Active Task</h2>
+                <h5 className='w-1/5 font-medium text-lg '>New Task</h5>
+                <h5 className='w-1/5 font-medium text-lg '>Completed Task</h5>
+                <h5 className='w-1/5 font-medium text-lg '>Failed Task</h5>
             </div>
-
-            {authData.employees.map(function (elem) {
-                return <div className='bg-red-500 mb-2 flex justify-between rounded py-2 px-4'>
-                    <h2 className='w-1/5 bg-red-600'>{elem.firstName}</h2>
-                    <h3 className='w-1/5 bg-red-600 '>task</h3>
-                    <h5 className='w-1/5 bg-red-600'>Status</h5>
-                    <h5 className='w-1/5 bg-red-600'>Status</h5>
-                    <h5 className='w-1/5 bg-red-600'>Failed</h5>
-                </div>
-            })}
+            <div id='random' className='h-[80%]'>
+                {authData.employees.map(function (elem, key) {
+                    return <div key={key} className=' mb-2 flex justify-between py-2 px-4 border-[1px] border-emerald-500 rounded-md'>
+                        <h2 className='w-1/5'>{elem.firstName}</h2>
+                        <h3 className='w-1/5 text-yellow-300 '>{elem.taskCounts.active}</h3>
+                        <h5 className='w-1/5 text-sky-400'>{elem.taskCounts.newTask}</h5>
+                        <h5 className='w-1/5 text-green-500'>{elem.taskCounts.completed}</h5>
+                        <h5 className='w-1/5 text-red-600'>{elem.taskCounts.failed}</h5>
+                    </div>
+                })}
+            </div>
         </div>
     )
 }
--- src/components/Others/CreateTask.jsx
+++ src/components/Others/CreateTask.jsx
@@ -1,28 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreateTask = () => {
+
+
+    const [taskTitle, setTaskTitle] = useState('')
+    const [taskDescription, setTaskDescription] = useState('')
+    const [taskDate, setTaskDate] = useState('')
+    const [assignTo, setAssignTo] = useState('')
+    const [category, setCategory] = useState('')
+
+
+    const [task, setTask] = useState({})
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        setTask({
+            taskTitle,
+            taskDate,
+            taskDescription,
+            category,
+            active: false,
+            newtask: true,
+            failed: true,
+            completed: false
+        })
+
+        setTaskDate("");
+        setAssignTo("");
+        setCategory("");
+        setTaskDescription("");
+        setTaskTitle("");
+    }
+
     return (
         <div className='p-5 bg-[#1c1c1c] mt-7 rounded'>
-            <form className='flex flex-wrap w-full items-start justify-between'>
+            <form onSubmit={(e) => {
+                submitHandler(e)
+            }} className='flex flex-wrap w-full items-start justify-between'>
                 <div className='w-1/2'>
                     <div>
                         <h3 className='text-sm text-gray-300 mb-0.5'>Task Title</h3>
-                        <input className='mb-4 text-sm py-1 px-2 w-2/5 rounded outline-none bg-transparent border-[1px] border-gray-400' type="text" placeholder='Make a task' />
+                        <input value={taskTitle}
+                            onChange={(e) => {
+                                setTaskTitle(e.target.value)
+                            }} className='mb-4 text-sm py-1 px-2 w-2/5 rounded outline-none bg-transparent border-[1px] border-gray-400' type="text" placeholder='Make a task' />
                     </div>
                     <div><h3 className='text-sm text-gray-300 mb-0.5'>Date</h3>
-                        <input className='mb-4 text-sm py-1 px-2 w-2/5 rounded outline-none text-gray-300 bg-transparent border-gray-400 border-[1px]' type="date" name="" id="" />
+                        <input value={taskDate}
+                            onChange={(e) => {
+                                setTaskDate(e.target.value)
+                            }} className='mb-4 text-sm py-1 px-2 w-2/5 rounded outline-none text-gray-300 bg-transparent border-gray-400 border-[1px]' type="date" name="" id="" />
                     </div>
                     <div> <h3 className='text-sm text-gray-300 mb-0.5'>Assign To</h3>
-                        <input className=' mb-4 text-sm py-1 px-2 w-2/5 rounded outline-none bg-transparent border-[1px] border-gray-400' type="text" placeholder='Employee Name' id="" />
+                        <input value={assignTo}
+                            onChange={(e) => {
+                                setAssignTo(e.target.value)
+                            }} className=' mb-4 text-sm py-1 px-2 w-2/5 rounded outline-none bg-transparent border-[1px] border-gray-400' type="text" placeholder='Employee Name' id="" />
                     </div>
-                    <div><h3 className='text-sm text-gray-300 mb-0.5'>Company</h3>
-                        <input className=' mb-4 text-sm py-1 px-2 w-2/5 rounded outline-none bg-transparent border-[1px] border-gray-400' type="text" placeholder='design, development, etc' id="" />
+                    <div><h3 className='text-sm text-gray-300 mb-0.5'>Category</h3>
+                        <input value={category}
+                            onChange={(e) => {
+                                setCategory(e.target.value)
+                            }} className=' mb-4 text-sm py-1 px-2 w-2/5 rounded outline-none bg-transparent border-[1px] border-gray-400' type="text" placeholder='design, development, etc' id="" />
                     </div>
                 </div>
 
                 <div className='w-2/5 flex flex-col items-start '>
                     <h3 className='text-sm text-gray-300 mb-0.5'>Description</h3>
-                    <textarea className=' w-full h-44 text-sm py-2 px-4 rounded border-gray-400 outline-none bg-transparent border-[1px]' name="" id="" cols="30" rows="10"></textarea>
+                    <textarea value={taskDescription}
+                        onChange={(e) => {
+                            setTaskDescription(e.target.value)
+                        }} className=' w-full h-44 text-sm py-2 px-4 rounded border-gray-400 outline-none bg-transparent border-[1px]' name="" id="" cols="30" rows="10"></textarea>
                     <button className='bg-emerald-600 p-3 rounded-md mt-4 text-lg font-medium w-1/2'>Create Task</button>
                 </div>
 
